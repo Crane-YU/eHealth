aeb0244c2a611d6490723b3307cdf3539100f5df
craneyu <jianhuiyu2@gmail.com>
2018-10-07 10:21:20 +1100

    Arduino to python code change

--- web/test_arduino.py
+++ web/test_arduino.py
@@ -6,9 +6,17 @@ ser = serial.Serial('/dev/cu.usbmodem14611', 115200)  # Establish the connection
 while True:
     # read the data from the arduino
     data = ser.readline()
-    # split the data by the tab
-    # pieces = data.split("\t")
+
     # check the data type
     print(type(data))
+
     # check the raw data value
     print(data)
+
+    # decode the byte type data to string
+    # data = data.decode("utf-8", "ignore")
+
+    # split the data by the tab
+    # pieces = data.split("\t")
+    # print(pieces)
+
